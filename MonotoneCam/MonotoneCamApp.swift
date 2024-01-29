cdf303d7f96c34191b3cd27826138bfc0fecbe6e
ootanl <6120nashville@gmail.com>
2024-01-29 16:23:31 +0900

    Initial Commit

---
+++ MonotoneCam/MonotoneCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonotoneCamApp.swift
+//  MonotoneCam
+//
+//  Created by ootani yoshiyuki on 2024/01/29.
+//
+
+import SwiftUI
+
+@main
+struct MonotoneCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
